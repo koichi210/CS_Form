83c641ad09c0bd25b8dda6cb9ddf8ebefbc42bb5
koichi <koichi210@gmail.com>
2020-11-29 00:36:33 +0900

    Add files via upload

---
+++ BackGroundWorker/BackGroundWorker/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace BackGroundWorker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(51, 64);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 25);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(245, 23);
+            this.progressBar.TabIndex = 1;
+            // 
+            // bgWorker
+            // 
+            this.bgWorker.WorkerReportsProgress = true;
+            this.bgWorker.WorkerSupportsCancellation = true;
+            this.bgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorker_DoWork_1);
+            this.bgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgWorker_ProgressChanged_1);
+            this.bgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorker_RunWorkerCompleted_1);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(143, 64);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 2;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 109);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button buttonStart;
+        public System.Windows.Forms.ProgressBar progressBar;
+        public System.ComponentModel.BackgroundWorker bgWorker;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
+
---
+++ BackGroundWorker/BackGroundWorker/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BackGroundWorker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // [排他制御1]実行中かどうかをIsBusyで制御する場合
+            if (bgWorker.IsBusy)
+            {
+                MessageBox.Show("処理中です");
+                return;
+            }
+
+            // [排他制御2]実行中にボタンが押せないように制御する場合
+            buttonStart.Enabled = false;
+            buttonCancel.Enabled = true;
+
+            // 別スレッドを非同期実行
+            List<object> arguments = new List<object>();
+            arguments.Add(100);
+
+            // 別スレッドを非同期実行
+            bgWorker.RunWorkerAsync(arguments);   // ⇒DoWork()
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            bgWorker.CancelAsync();
+        }
+
+        private void bgWorker_DoWork_1(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            // 別スレッドで実行されるため、このメソッドではGUIを操作してはいけない
+
+            // senderの値はbgWorkerの値と同じ
+            BackgroundWorker worker = (BackgroundWorker)sender;
+
+            // このメソッドへのパラメータ
+            List<object> genericlist = e.Argument as List<object>;
+
+            int Param1 = (int)genericlist[0]; // 100
+
+            // 時間のかかる処理
+            for (int i = 0; i < Param1; i++)
+            {
+                System.Threading.Thread.Sleep(100);
+
+                int percentage = 100 * i / Param1;      // 進捗率
+                worker.ReportProgress(percentage);      // ⇒ProgressChanged()
+
+                // キャンセルされてないかチェック
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+
+            // このメソッドからの戻り値
+            e.Result = "すべて完了";
+
+            // ⇒RunWorkerCompleted()
+        }
+
+        private void bgWorker_ProgressChanged_1(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            // 進捗率の表示
+            this.Text = e.ProgressPercentage + "％完了";
+            progressBar.Value = e.ProgressPercentage;
+        }
+
+        private void bgWorker_RunWorkerCompleted_1(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("キャンセルされました");
+                // この場合にはe.Resultにはアクセスできない
+            }
+            else
+            {
+                // 処理結果の表示
+                this.Text = e.Result.ToString();
+                MessageBox.Show("正常に完了");
+            }
+
+            buttonStart.Enabled = true;
+            buttonCancel.Enabled = false;
+        }
+    }
+}
