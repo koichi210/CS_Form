c775186405d7356900024c5385034860f1f6ed3e
koichi <koichi210@gmail.com>
2020-11-29 08:56:41 +0900

    Add files via upload

---
+++ StrCompare/StrCompare/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace StrCompare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxSource = new System.Windows.Forms.TextBox();
+            this.textBoxTarget = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(171, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "比較";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxSource
+            // 
+            this.textBoxSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSource.Location = new System.Drawing.Point(37, 12);
+            this.textBoxSource.Name = "textBoxSource";
+            this.textBoxSource.Size = new System.Drawing.Size(209, 25);
+            this.textBoxSource.TabIndex = 1;
+            // 
+            // textBoxTarget
+            // 
+            this.textBoxTarget.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxTarget.Location = new System.Drawing.Point(37, 43);
+            this.textBoxTarget.Name = "textBoxTarget";
+            this.textBoxTarget.Size = new System.Drawing.Size(209, 25);
+            this.textBoxTarget.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 135);
+            this.Controls.Add(this.textBoxTarget);
+            this.Controls.Add(this.textBoxSource);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxSource;
+        private System.Windows.Forms.TextBox textBoxTarget;
+    }
+}
+
---
+++ StrCompare/StrCompare/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace StrCompare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxSource.Text = "SampleString";
+            textBoxTarget.Text = "samplestring";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Compare(textBoxSource.Text, textBoxTarget.Text);
+            //SampleCompare();
+        }
+
+        private void Compare(String Source, String Target)
+        {
+            String ResultStr = "";
+
+            // 大文字・小文字は区別される（完全一致）
+            Boolean ret = Source.Equals(Target);
+            ResultStr += "大文字小文字区別する（完全一致） =" + ret.ToString() + Environment.NewLine;
+
+            // 大文字・小文字を区別しない（それ以外は完全一致）
+            ret = Source.Equals(Target, StringComparison.OrdinalIgnoreCase);
+            ResultStr += "大文字小文字区別しない（完全一致） =" + ret.ToString() + Environment.NewLine;
+
+            // 大文字・小文字を区別しない（前方一致で比較）
+            ret = Source.StartsWith(Target, StringComparison.OrdinalIgnoreCase);
+            ResultStr += "大文字小文字区別しない（前方一致） =" + ret.ToString() + Environment.NewLine;
+
+            MessageBox.Show(ResultStr);
+        }
+
+        private void SampleCompare()
+        {
+            String Source = "sampleString";
+            String ResultStr = "";
+
+            // 大文字・小文字は区別される（完全一致）
+            Boolean ret = Source.Equals("sampleString");
+            ResultStr += "[" + Source + "][" + "sampleString" + "]" + Environment.NewLine;
+            ResultStr += "大文字小文字区別する（完全一致） =" + ret.ToString() + Environment.NewLine + Environment.NewLine;
+
+            // 大文字・小文字は区別される（完全一致）
+            ret = Source.Equals("sampleSTRING");
+            ResultStr += "[" + Source + "][" + "sampleSTRING" + "]" + Environment.NewLine;
+            ResultStr += "大文字小文字区別する（完全一致） =" + ret.ToString() + Environment.NewLine + Environment.NewLine;
+
+            // 大文字・小文字を区別しない（それ以外は完全一致）
+            ret = Source.Equals("sampleSTRING", StringComparison.OrdinalIgnoreCase);
+            ResultStr += "[" + Source + "][" + "sampleSTRING" + "]" + Environment.NewLine;
+            ResultStr += "大文字小文字区別しない（完全一致） =" + ret.ToString() + Environment.NewLine + Environment.NewLine;
+
+            // 大文字・小文字を区別しない（前方一致で比較）
+            ret = Source.StartsWith("SAMPLE", StringComparison.OrdinalIgnoreCase);
+            ResultStr += "[" + Source + "][" + "SAMPLE" + "]" + Environment.NewLine;
+            ResultStr += "大文字小文字区別しない（前方一致） =" + ret.ToString() + Environment.NewLine + Environment.NewLine;
+
+            MessageBox.Show(ResultStr);
+        }
+    }
+}
