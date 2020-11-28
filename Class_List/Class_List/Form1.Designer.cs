921baebc9ea1a42b8da0ad9c3dac8c792f856c72
koichi <koichi210@gmail.com>
2020-11-29 00:39:59 +0900

    Add files via upload

---
+++ Class_List/Class_List/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Class_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBox_num = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonRestore = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(47, 64);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBox_num
+            // 
+            this.textBox_num.Location = new System.Drawing.Point(54, 25);
+            this.textBox_num.Name = "textBox_num";
+            this.textBox_num.Size = new System.Drawing.Size(68, 19);
+            this.textBox_num.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "回数";
+            // 
+            // buttonRestore
+            // 
+            this.buttonRestore.Location = new System.Drawing.Point(47, 93);
+            this.buttonRestore.Name = "buttonRestore";
+            this.buttonRestore.Size = new System.Drawing.Size(75, 23);
+            this.buttonRestore.TabIndex = 3;
+            this.buttonRestore.Text = "Restore";
+            this.buttonRestore.UseVisualStyleBackColor = true;
+            this.buttonRestore.Click += new System.EventHandler(this.buttonRestore_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.buttonRestore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_num);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBox_num;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonRestore;
+    }
+}
+
---
+++ Class_List/Class_List/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Class_List
+{
+    public partial class Form1 : Form
+    {
+        public struct Table
+        {
+            public int Group;
+            public String SrcName;
+            public String DestName;
+
+            public Table(int idx, string srcName, string destName)
+            {
+                Group = idx;
+                SrcName = srcName;
+                DestName = destName;
+            }
+        }
+
+        public enum INDEX_COUNTER
+        {
+            INCREMENT,
+            DECREMENT
+        };
+
+
+        public void UpdateIdx(INDEX_COUNTER cnt)
+        {
+            switch (cnt)
+            {
+                case INDEX_COUNTER.INCREMENT:
+                    LastIdx++;
+                    break;
+
+                case INDEX_COUNTER.DECREMENT:
+                    if (LastIdx > 0)
+                    {
+                        LastIdx--;
+                    }
+                    break;
+            }
+        }
+
+        private int LastIdx = 0;
+        private List<Table> AllList = new List<Table>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < int.Parse(textBox_num.Text); i++)
+            {
+                AddList(AllList, i);
+            }
+            UpdateIdx(INDEX_COUNTER.INCREMENT);
+
+            ResultDump();
+        }
+
+        private void buttonRestore_Click(object sender, EventArgs e)
+        {
+            UpdateIdx(INDEX_COUNTER.DECREMENT);
+            for (int i = AllList.Count - 1; i > 0; i--)
+            {
+                if (!SubList(AllList, i))
+                {
+                    break;
+                }
+            }
+
+            ResultDump();
+        }
+
+        private void AddList(List<Table> arr, int num)
+        {
+            arr.Add(new Table(LastIdx, "Source" + num.ToString(), "Destination" + num.ToString()));
+        }
+
+        private Boolean SubList(List<Table> arr, int num)
+        {
+            if (arr[num].Group == LastIdx)
+            {
+                arr.RemoveAt(num);
+                return true;
+            }
+            return false;
+        }
+
+        private void ResultDump()
+        {
+            String Result = "";
+            for (int i = 0; i < AllList.Count; i++)
+            {
+                Result += "[" + i.ToString() + "]" + Environment.NewLine;
+                Result += "  Group=    " + AllList[i].Group + Environment.NewLine;
+                Result += "  SrcName= " + AllList[i].SrcName + Environment.NewLine;
+                Result += "  DestName=" + AllList[i].DestName + Environment.NewLine;
+            }
+            MessageBox.Show(Result, "Tableの中身");
+        }
+    }
+}
