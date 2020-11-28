f2f5598b2449347025d647e9d69dc3822e3796b1
koichi <koichi210@gmail.com>
2020-11-29 00:59:58 +0900

    Add files via upload

---
+++ DialogChild/DialogChild/FormChild.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogChild
+{
+    public partial class FormChild : Form
+    {
+        public FormChild()
+        {
+            InitializeComponent();
+        }
+    }
+}
