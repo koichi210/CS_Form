38e340d0d88010c2c6a8fc27d03e28cc4870ba6a
koichi <koichi210@gmail.com>
2020-11-29 01:03:08 +0900

    Add files via upload

---
+++ EventRecorder/EventRecorder/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace EventRecorder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Setting = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Setting
+            // 
+            this.button_Setting.Location = new System.Drawing.Point(59, 12);
+            this.button_Setting.Name = "button_Setting";
+            this.button_Setting.Size = new System.Drawing.Size(51, 23);
+            this.button_Setting.TabIndex = 0;
+            this.button_Setting.Text = "設定";
+            this.button_Setting.UseVisualStyleBackColor = true;
+            this.button_Setting.Click += new System.EventHandler(this.button_Setting_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "MouseHook";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(35, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "KeyHook";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(33, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Play";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 10);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(164, 144);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(10, 135);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 19);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 169);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_Setting);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "EventRecorder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Setting;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ EventRecorder/EventRecorder/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using StandardTemplate;
+
+namespace EventRecorder
+{
+    public partial class Form1 : Form
+    {
+        private StcUtils util = new StcUtils();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Icon = Properties.Resources.EventRecorder;
+            util.SetCurrentDirectory();
+        }
+
+        private void button_Setting_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ごめんね。作成中。。");
+        }
+
+        void hookMouseTest(ref GlobalHook.MouseHook.StateMouse s)
+        {
+            textBox2.Text = s.X + ", " + s.Y;
+
+            if (s.Stroke == GlobalHook.MouseHook.Stroke.X1_DOWN)
+            {
+                GlobalHook.MouseHook.Cancel();
+                textBox1.Text = "Disable X1_DOWN" + "\r\n" + textBox1.Text;
+                return;
+            }
+
+            if (s.Stroke != GlobalHook.MouseHook.Stroke.MOVE)
+            {
+                textBox1.Text = s.Stroke + "\r\n" + textBox1.Text;
+            }
+        }
+
+        void hookKeyboardTest(ref GlobalHook.KeyboardHook.StateKeyboard s)
+        {
+            textBox1.Text = s.Stroke + " : " + s.Key + "\r\n" + textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (GlobalHook.MouseHook.IsHooking)
+            {
+                GlobalHook.MouseHook.Stop();
+                return;
+            }
+
+            GlobalHook.MouseHook.AddEvent(hookMouseTest);
+            GlobalHook.MouseHook.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (GlobalHook.KeyboardHook.IsHooking)
+            {
+                GlobalHook.KeyboardHook.Stop();
+                return;
+            }
+
+            GlobalHook.KeyboardHook.AddEvent(hookKeyboardTest);
+            GlobalHook.KeyboardHook.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<EventRecorder.Windows.Simulation.InputSimulator.Input> inputs = new List<EventRecorder.Windows.Simulation.InputSimulator.Input>();
+            List<EventRecorder.Windows.Simulation.InputSimulator.MouseStroke> flags = new List<EventRecorder.Windows.Simulation.InputSimulator.MouseStroke>();
+
+            flags.Add(EventRecorder.Windows.Simulation.InputSimulator.MouseStroke.LEFT_DOWN);
+            flags.Add(EventRecorder.Windows.Simulation.InputSimulator.MouseStroke.LEFT_UP);
+            flags.Add(EventRecorder.Windows.Simulation.InputSimulator.MouseStroke.MOVE);
+
+            EventRecorder.Windows.Simulation.InputSimulator.AddMouseInput(ref inputs, flags, 0, false, 0, 50);
+            EventRecorder.Windows.Simulation.InputSimulator.AddKeyboardInput(ref inputs, "ゆっくりしていってね！！");
+
+            EventRecorder.Windows.Simulation.InputSimulator.SendInput(inputs);
+        }
+
+    }
+}
