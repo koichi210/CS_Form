7206fa1f14c1e2d3c29685b1782ba7ed38a136cb
koichi <koichi210@gmail.com>
2020-11-29 01:06:11 +0900

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace FizzBuzz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Number = new System.Windows.Forms.TextBox();
+            this.button_execute = new System.Windows.Forms.Button();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_Number
+            // 
+            this.textBox_Number.Location = new System.Drawing.Point(19, 29);
+            this.textBox_Number.Name = "textBox_Number";
+            this.textBox_Number.Size = new System.Drawing.Size(56, 19);
+            this.textBox_Number.TabIndex = 0;
+            // 
+            // button_execute
+            // 
+            this.button_execute.Location = new System.Drawing.Point(81, 29);
+            this.button_execute.Name = "button_execute";
+            this.button_execute.Size = new System.Drawing.Size(59, 23);
+            this.button_execute.TabIndex = 1;
+            this.button_execute.Text = "実行";
+            this.button_execute.UseVisualStyleBackColor = true;
+            this.button_execute.Click += new System.EventHandler(this.button_execute_Click);
+            // 
+            // textBox_Result
+            // 
+            this.textBox_Result.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Result.Location = new System.Drawing.Point(158, 29);
+            this.textBox_Result.Multiline = true;
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Result.Size = new System.Drawing.Size(94, 140);
+            this.textBox_Result.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "いくつまで表示しますか？";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "動作結果";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 189);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Result);
+            this.Controls.Add(this.button_execute);
+            this.Controls.Add(this.textBox_Number);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Number;
+        private System.Windows.Forms.Button button_execute;
+        private System.Windows.Forms.TextBox textBox_Result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ FizzBuzz/FizzBuzz/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FizzBuzz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_Number.Text = "100";
+        }
+
+        private void button_execute_Click(object sender, EventArgs e)
+        {
+            if (textBox_Number.Text == String.Empty)
+            {
+                MessageBox.Show("pls set number");
+                return;
+            }
+            textBox_Result.Text = FizzBuzz(int.Parse(textBox_Number.Text));
+
+        }
+
+        private String FizzBuzz(int Number)
+        {
+            String Result = "";
+
+            for (int i = 1; i <= Number; i++)
+            {
+                Result += i.ToString() + " : ";
+
+                String Add = "";
+                if (i % 3 == 0)
+                {
+                    Add += "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    Add += "Buzz";
+                }
+                if (i % 7 == 0)
+                {
+                    Add += "Woof";
+                }
+
+                if (Add == String.Empty)
+                {
+                    Add = i.ToString();
+                }
+
+                Result += Add + System.Environment.NewLine;
+            }
+
+            return Result;
+        }
+    }
+}
