f1a246537300a4d0f2b627e7c1a17fa7a1ef0be2
koichi <koichi210@gmail.com>
2020-11-29 01:06:39 +0900

    Add files via upload

---
+++ Graphics/Graphics/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Graphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.buttonBarGraph = new System.Windows.Forms.Button();
+            this.buttonCircleGraph = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(22, 12);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // buttonBarGraph
+            // 
+            this.buttonBarGraph.Location = new System.Drawing.Point(166, 318);
+            this.buttonBarGraph.Name = "buttonBarGraph";
+            this.buttonBarGraph.Size = new System.Drawing.Size(75, 23);
+            this.buttonBarGraph.TabIndex = 1;
+            this.buttonBarGraph.Text = "棒グラフ";
+            this.buttonBarGraph.UseVisualStyleBackColor = true;
+            this.buttonBarGraph.Click += new System.EventHandler(this.buttonBarGraph_Click);
+            // 
+            // buttonCircleGraph
+            // 
+            this.buttonCircleGraph.Location = new System.Drawing.Point(85, 318);
+            this.buttonCircleGraph.Name = "buttonCircleGraph";
+            this.buttonCircleGraph.Size = new System.Drawing.Size(75, 23);
+            this.buttonCircleGraph.TabIndex = 2;
+            this.buttonCircleGraph.Text = "円グラフ";
+            this.buttonCircleGraph.UseVisualStyleBackColor = true;
+            this.buttonCircleGraph.Click += new System.EventHandler(this.buttonCircleGraph_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(247, 318);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 3;
+            this.Delete.Text = "消去";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 400);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.buttonCircleGraph);
+            this.Controls.Add(this.buttonBarGraph);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button buttonBarGraph;
+        private System.Windows.Forms.Button buttonCircleGraph;
+        private System.Windows.Forms.Button Delete;
+    }
+}
+
---
+++ Graphics/Graphics/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Graphics
+{
+    public partial class Form1 : Form
+    {
+        private int StartBarPoint = 0;  // 棒グラフの描画基点（描画するたびに基点をインクリ）
+
+        private Series series;
+        private Chart chart;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBarGraph_Click(object sender, EventArgs e)
+        {
+            //Seriesの作成
+            series = new Series();
+
+            //グラフのタイプを指定(今回は線)
+            series.ChartType = SeriesChartType.Line;
+
+            //グラフのデータを追加(試しにsin関数)
+            for (int i = 0; i < 360; i++)
+            {
+                series.Points.AddXY(i, Math.Sin((i - StartBarPoint) * Math.PI / 180.0));
+            }
+
+            //作ったSeriesをchartコントロールに追加する
+            chart1.Series.Add(series);
+
+            StartBarPoint += 10;
+        }
+
+        private void buttonCircleGraph_Click(object sender, EventArgs e)
+        {
+            chart = new Chart();
+
+            chart.Width = 200;
+            chart.Height = 200;
+
+            series = new Series();
+            series.ChartType = SeriesChartType.Pie;
+            series["PieStartAngle"] = "270";
+
+            DataPoint point = new DataPoint();
+            point.XValue = 0;
+            point.YValues = new double[] { 30 };
+            point.Color = System.Drawing.Color.Red;
+            //point.BackSecondaryColor = System.Drawing.Color.DarkRed;
+            //point.BackGradientStyle = GradientStyle.DiagonalLeft;
+            series.Points.Add(point);
+
+            point = new DataPoint();
+            point.XValue = 0;
+            point.YValues = new double[] { 10 };
+            point.Color = System.Drawing.Color.Khaki;
+            series.Points.Add(point);
+
+            point = new DataPoint();
+            point.XValue = 0;
+            point.YValues = new double[] { 20 };
+            point.Color = System.Drawing.Color.AliceBlue;
+            series.Points.Add(point);
+
+            point = new DataPoint();
+            point.XValue = 0;
+            point.YValues = new double[] { 20 };
+            series.Points.Add(point);
+
+            point = new DataPoint();
+            point.XValue = 0;
+            point.YValues = new double[] { 20 };
+            series.Points.Add(point);
+
+            chart.Series.Add(series);
+
+            ChartArea area = new ChartArea();
+            area.AxisX.IsLabelAutoFit = true;
+            area.AxisY.IsLabelAutoFit = true;
+            chart.ChartAreas.Add(area);
+
+            chart1.Controls.Add(chart);
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            //chart1.Controls.Remove(chart);
+
+            if (chart1.Controls.Count > 0)
+            {
+                chart1.Controls.RemoveAt(0);
+            }
+
+            if (chart1.Series.Count > 0)
+            {
+                chart1.Series.RemoveAt(0);
+            }
+        }
+    }
+}
