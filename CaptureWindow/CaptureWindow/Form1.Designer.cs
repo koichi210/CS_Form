b1fd5f0aeb74aabb2b8195a5f45c09cdadabb08a
koichi <koichi210@gmail.com>
2020-11-29 00:38:50 +0900

    Add files via upload

---
+++ CaptureWindow/CaptureWindow/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace CaptureWindow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button_Capture = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBox_SavePath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TextBox_MouseX = new System.Windows.Forms.TextBox();
+            this.TextBox_MouseY = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Radio_FullScreen = new System.Windows.Forms.RadioButton();
+            this.Radio_CurrentScreen = new System.Windows.Forms.RadioButton();
+            this.Radio_CurrentWindow = new System.Windows.Forms.RadioButton();
+            this.TextBox_MousePoint = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TextBox_Sleep = new System.Windows.Forms.TextBox();
+            this.SaveSetting = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Button_Capture
+            // 
+            this.Button_Capture.Location = new System.Drawing.Point(12, 196);
+            this.Button_Capture.Name = "Button_Capture";
+            this.Button_Capture.Size = new System.Drawing.Size(219, 23);
+            this.Button_Capture.TabIndex = 20;
+            this.Button_Capture.Text = "Capture";
+            this.Button_Capture.UseVisualStyleBackColor = true;
+            this.Button_Capture.Click += new System.EventHandler(this.Button_Capture_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "保存フォルダパス：";
+            // 
+            // TextBox_SavePath
+            // 
+            this.TextBox_SavePath.Location = new System.Drawing.Point(12, 28);
+            this.TextBox_SavePath.Name = "TextBox_SavePath";
+            this.TextBox_SavePath.Size = new System.Drawing.Size(219, 19);
+            this.TextBox_SavePath.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "クリックする座標：";
+            // 
+            // TextBox_MouseX
+            // 
+            this.TextBox_MouseX.Location = new System.Drawing.Point(168, 78);
+            this.TextBox_MouseX.Name = "TextBox_MouseX";
+            this.TextBox_MouseX.Size = new System.Drawing.Size(50, 19);
+            this.TextBox_MouseX.TabIndex = 10;
+            // 
+            // TextBox_MouseY
+            // 
+            this.TextBox_MouseY.Location = new System.Drawing.Point(168, 103);
+            this.TextBox_MouseY.Name = "TextBox_MouseY";
+            this.TextBox_MouseY.Size = new System.Drawing.Size(50, 19);
+            this.TextBox_MouseY.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "X：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(145, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Y：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Capture対象：";
+            // 
+            // Radio_FullScreen
+            // 
+            this.Radio_FullScreen.AutoSize = true;
+            this.Radio_FullScreen.Location = new System.Drawing.Point(28, 81);
+            this.Radio_FullScreen.Name = "Radio_FullScreen";
+            this.Radio_FullScreen.Size = new System.Drawing.Size(77, 16);
+            this.Radio_FullScreen.TabIndex = 2;
+            this.Radio_FullScreen.TabStop = true;
+            this.Radio_FullScreen.Text = "FullScreen";
+            this.Radio_FullScreen.UseVisualStyleBackColor = true;
+            // 
+            // Radio_CurrentScreen
+            // 
+            this.Radio_CurrentScreen.AutoSize = true;
+            this.Radio_CurrentScreen.Location = new System.Drawing.Point(28, 102);
+            this.Radio_CurrentScreen.Name = "Radio_CurrentScreen";
+            this.Radio_CurrentScreen.Size = new System.Drawing.Size(96, 16);
+            this.Radio_CurrentScreen.TabIndex = 3;
+            this.Radio_CurrentScreen.TabStop = true;
+            this.Radio_CurrentScreen.Text = "CurrentScreen";
+            this.Radio_CurrentScreen.UseVisualStyleBackColor = true;
+            // 
+            // Radio_CurrentWindow
+            // 
+            this.Radio_CurrentWindow.AutoSize = true;
+            this.Radio_CurrentWindow.Location = new System.Drawing.Point(28, 124);
+            this.Radio_CurrentWindow.Name = "Radio_CurrentWindow";
+            this.Radio_CurrentWindow.Size = new System.Drawing.Size(99, 16);
+            this.Radio_CurrentWindow.TabIndex = 4;
+            this.Radio_CurrentWindow.TabStop = true;
+            this.Radio_CurrentWindow.Text = "CurrentWindow";
+            this.Radio_CurrentWindow.UseVisualStyleBackColor = true;
+            // 
+            // TextBox_MousePoint
+            // 
+            this.TextBox_MousePoint.Location = new System.Drawing.Point(12, 225);
+            this.TextBox_MousePoint.Name = "TextBox_MousePoint";
+            this.TextBox_MousePoint.ReadOnly = true;
+            this.TextBox_MousePoint.Size = new System.Drawing.Size(59, 19);
+            this.TextBox_MousePoint.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(134, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 12);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Sleep時間（Sec）：";
+            // 
+            // TextBox_Sleep
+            // 
+            this.TextBox_Sleep.Location = new System.Drawing.Point(168, 160);
+            this.TextBox_Sleep.Name = "TextBox_Sleep";
+            this.TextBox_Sleep.Size = new System.Drawing.Size(50, 19);
+            this.TextBox_Sleep.TabIndex = 23;
+            // 
+            // SaveSetting
+            // 
+            this.SaveSetting.Location = new System.Drawing.Point(153, 225);
+            this.SaveSetting.Name = "SaveSetting";
+            this.SaveSetting.Size = new System.Drawing.Size(75, 23);
+            this.SaveSetting.TabIndex = 24;
+            this.SaveSetting.Text = "設定値保存";
+            this.SaveSetting.UseVisualStyleBackColor = true;
+            this.SaveSetting.Click += new System.EventHandler(this.SaveSetting_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 253);
+            this.Controls.Add(this.SaveSetting);
+            this.Controls.Add(this.TextBox_Sleep);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TextBox_MousePoint);
+            this.Controls.Add(this.Radio_CurrentWindow);
+            this.Controls.Add(this.Radio_CurrentScreen);
+            this.Controls.Add(this.Radio_FullScreen);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TextBox_MouseY);
+            this.Controls.Add(this.TextBox_MouseX);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TextBox_SavePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Button_Capture);
+            this.Name = "Form1";
+            this.Text = "CaptureWindow";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button_Capture;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBox_SavePath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TextBox_MouseX;
+        private System.Windows.Forms.TextBox TextBox_MouseY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton Radio_FullScreen;
+        private System.Windows.Forms.RadioButton Radio_CurrentScreen;
+        private System.Windows.Forms.RadioButton Radio_CurrentWindow;
+        private System.Windows.Forms.TextBox TextBox_MousePoint;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TextBox_Sleep;
+        private System.Windows.Forms.Button SaveSetting;
+    }
+}
+
---
+++ CaptureWindow/CaptureWindow/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Xml;
+
+namespace CaptureWindow
+{
+    public partial class Form1 : Form
+    {
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        extern static uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct INPUT
+        {
+            public int type;
+            public MOUSEINPUT mi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public int mouseData;
+            public int dwFlags;
+            public int time;
+            public IntPtr dwExtraInfo;
+        }
+
+        const int MOUSEEVENTF_MOVE      = 0x0001;   // 移動
+        const int MOUSEEVENTF_LEFTDOWN  = 0x0002;   // 左ボタン Down
+        const int MOUSEEVENTF_LEFTUP    = 0x0004;   // 左ボタン Up
+        const int MOUSEEVENTF_ABSOLUTE  = 0x8000;   // 絶対値指定
+        ///////////////////////////////////////////////////////////////////////////////////////////////
+
+        String SaveFileName;
+        readonly String SaveXmlFile = @"CaptureWindow.xml";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            TextBox_SavePath.Text = @"c:\tmp";
+            Radio_FullScreen.Checked = true;
+            TextBox_MouseX.Text = @"500";
+            TextBox_MouseY.Text = @"500";
+            TextBox_Sleep.Text = @"3";
+
+            LoadSetting();
+        }
+
+        private void Button_Capture_Click(object sender, EventArgs e)
+        {
+            if (!IsExistSavePath())
+            {
+                return;
+            }
+
+            String FileFormat = TextBox_SavePath.Text + @"\" + System.DateTime.Now.ToString("yyyy_dd_mm_HH_mm_ss");
+
+            SaveFileName = FileFormat + "_1.png";
+            CaptureProc();
+
+            if (!TextBox_MouseX.Text.Equals("") && !TextBox_MouseY.Text.Equals(""))
+            {
+                SaveFileName = FileFormat + "_2.png";
+                MouseProc();
+
+                if (!TextBox_Sleep.Text.Equals(""))
+                {
+                    System.Threading.Thread.Sleep(int.Parse(TextBox_Sleep.Text)*1000);
+                }
+                CaptureProc();
+            }
+        }
+
+        private bool IsExistSavePath()
+        {
+            bool IsExist = true;
+            if (! System.IO.Directory.Exists(TextBox_SavePath.Text))
+            {
+                DialogResult result = MessageBox.Show("ディレクトリは存在しません。作成しますか？",
+                    "Warning",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Exclamation,
+                    MessageBoxDefaultButton.Button1);
+
+                if (result == DialogResult.Yes)
+                {
+                    System.IO.Directory.CreateDirectory(TextBox_SavePath.Text);
+                }
+                else
+                {
+                    IsExist = false;
+                }
+            }
+
+            return IsExist;
+        }
+
+        // キャプチャ処理
+        private void CaptureProc()
+        {
+            if (Radio_FullScreen.Checked == true)
+            {
+                SaveWithCaptureFullScreen();
+            }
+            else if (Radio_CurrentScreen.Checked == true)
+            {
+                SaveWithCaptureCurrentScreen();
+            }
+            else // Radio_CurrentWindow
+            {
+                SaveWithCaptureCurrentWindow();
+            }
+        }
+
+        // マウス処理
+        private void MouseProc()
+        {
+            Point MousePt = new Point(Cursor.Position.X, Cursor.Position.Y);
+
+            //マウス移動
+            System.Windows.Forms.Cursor.Position = new System.Drawing.Point(
+                                                    int.Parse(TextBox_MouseX.Text),
+                                                    int.Parse(TextBox_MouseY.Text));
+
+            //struct 配列の宣言
+            INPUT[] input = new INPUT[2];
+
+            //左ボタン Down
+            //input[0].mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_LEFTDOWN;
+            input[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+            //input[0].mi.dx = int.Parse(TextBox_MouseX.Text);
+            //input[0].mi.dy = int.Parse(TextBox_MouseY.Text);
+
+            //左ボタン Up
+            input[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+            //input[1].mi.dx = input[0].mi.dx;
+            //input[1].mi.dy = input[0].mi.dy;
+
+            //イベントの一括生成
+            SendInput(2, input, Marshal.SizeOf(input[0]));
+
+            // マウスの位置をもとに戻す
+            System.Windows.Forms.Cursor.Position = new System.Drawing.Point(MousePt.X, MousePt.Y);
+        }
+
+        private void SaveWithCaptureFullScreen()
+        {
+            SendKeys.SendWait("^{PRTSC}");      // 全画面
+            IDataObject d = Clipboard.GetDataObject();
+            if (d != null)
+            {
+                //ビットマップデータ形式に関連付けられているデータを取得
+                Image img = (Image)d.GetData(DataFormats.Bitmap);
+                if (img != null)
+                {
+                    img.Save(SaveFileName);
+                }
+            }
+        }
+
+        private void SaveWithCaptureCurrentWindow()
+        {
+            SendKeys.SendWait("%{PRTSC}");      // Current Windowのみ
+            IDataObject d = Clipboard.GetDataObject();
+            if (d != null)
+            {
+                //ビットマップデータ形式に関連付けられているデータを取得
+                Image img = (Image)d.GetData(DataFormats.Bitmap);
+                if (img != null)
+                {
+                    img.Save(SaveFileName);
+                }
+            }
+        }
+
+        private void SaveWithCaptureCurrentScreen()
+        {
+            Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                    Screen.PrimaryScreen.Bounds.Height);
+
+            //Graphicsの作成
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                //画面全体をコピーする
+                g.CopyFromScreen(new Point(0, 0), new Point(0, 0), bmp.Size);
+
+                //解放
+                g.Dispose();
+            }
+
+            // ファイル保存
+            bmp.Save(SaveFileName);
+            bmp.Dispose();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            TextBox_MousePoint.Text = Cursor.Position.X.ToString() + "," + Cursor.Position.Y.ToString();
+        }
+
+        private void SaveSetting_Click(object sender, EventArgs e)
+        {
+            XmlDocument document = new XmlDocument();
+
+            XmlDeclaration declaration = document.CreateXmlDeclaration("1.0", "UTF-8", null);  // XML宣言
+            XmlElement root = document.CreateElement("root");  // ルート要素
+
+            document.AppendChild(declaration);
+            document.AppendChild(root);
+
+            XmlElement element = document.CreateElement("Setting");
+            element.SetAttribute("attribute", "TextBox_SavePath");
+            element.InnerText = TextBox_SavePath.Text;
+            root.AppendChild(element);
+
+            element = document.CreateElement("Setting");
+            element.SetAttribute("attribute", "TextBox_MouseX");
+            element.InnerText = TextBox_MouseX.Text;
+            root.AppendChild(element);
+
+            element = document.CreateElement("Setting");
+            element.SetAttribute("attribute", "TextBox_MouseY");
+            element.InnerText = TextBox_MouseY.Text;
+            root.AppendChild(element);
+
+            element = document.CreateElement("Setting");
+            element.SetAttribute("attribute", "TextBox_Sleep");
+            element.InnerText = TextBox_Sleep.Text;
+            root.AppendChild(element);
+
+            // ファイルに保存する
+            document.Save(SaveXmlFile);
+
+            MessageBox.Show("設定値を保存しました♪");
+        }
+
+        private void LoadSetting()
+        {
+            if (!File.Exists(SaveXmlFile))
+            {
+                return;
+            }
+
+            // ファイルから読み込む
+            XmlDocument document = new XmlDocument();
+            document.Load(SaveXmlFile);
+
+            foreach (XmlElement element in document.DocumentElement)
+            {
+                string attribute = element.GetAttribute("attribute");   // 属性
+                string text = element.InnerText;                        // 要素の内容
+
+                if (attribute.Equals("TextBox_SavePath"))
+                {
+                    TextBox_SavePath.Text = text;
+                }
+                else if (attribute.Equals("TextBox_MouseX"))
+                {
+                    TextBox_MouseX.Text = text;
+                }
+                else if (attribute.Equals("TextBox_MouseY"))
+                {
+                    TextBox_MouseY.Text = text;
+                }
+                else if (attribute.Equals("TextBox_Sleep"))
+                {
+                    TextBox_Sleep.Text = text;
+                }
+            }
+        }
+    }
+}
