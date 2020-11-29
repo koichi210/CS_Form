3dae8f60a5f2e50bd258c1a5292f7cefe6500f44
koichi <koichi210@gmail.com>
2020-11-29 09:00:32 +0900

    Add files via upload

---
+++ Thumnail/Thumnail/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Thumnail
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox_Image = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Thumnail = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_FilePath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_Exe = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Image)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Thumnail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_Image
+            // 
+            this.pictureBox_Image.Location = new System.Drawing.Point(144, 83);
+            this.pictureBox_Image.Name = "pictureBox_Image";
+            this.pictureBox_Image.Size = new System.Drawing.Size(181, 121);
+            this.pictureBox_Image.TabIndex = 0;
+            this.pictureBox_Image.TabStop = false;
+            // 
+            // pictureBox_Thumnail
+            // 
+            this.pictureBox_Thumnail.Location = new System.Drawing.Point(12, 83);
+            this.pictureBox_Thumnail.Name = "pictureBox_Thumnail";
+            this.pictureBox_Thumnail.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox_Thumnail.TabIndex = 1;
+            this.pictureBox_Thumnail.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ファイル名：";
+            // 
+            // textBox_FilePath
+            // 
+            this.textBox_FilePath.Location = new System.Drawing.Point(75, 6);
+            this.textBox_FilePath.Name = "textBox_FilePath";
+            this.textBox_FilePath.Size = new System.Drawing.Size(250, 19);
+            this.textBox_FilePath.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "サムネイル";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(142, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "プレビュー";
+            // 
+            // button_Exe
+            // 
+            this.button_Exe.Location = new System.Drawing.Point(250, 31);
+            this.button_Exe.Name = "button_Exe";
+            this.button_Exe.Size = new System.Drawing.Size(75, 23);
+            this.button_Exe.TabIndex = 3;
+            this.button_Exe.Text = "表示";
+            this.button_Exe.UseVisualStyleBackColor = true;
+            this.button_Exe.Click += new System.EventHandler(this.button_Exe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 225);
+            this.Controls.Add(this.button_Exe);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_FilePath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox_Thumnail);
+            this.Controls.Add(this.pictureBox_Image);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = new System.Drawing.Size(345, 253);
+            this.Name = "Form1";
+            this.Text = "Thumnail";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Image)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Thumnail)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_Image;
+        private System.Windows.Forms.PictureBox pictureBox_Thumnail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_FilePath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_Exe;
+    }
+}
+
---
+++ Thumnail/Thumnail/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Thumnail
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_FilePath.Text = @"D:\sample.png";
+        }
+
+        private void button_Exe_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBox_FilePath.Text))
+            {
+                MessageBox.Show("ファイルが存在しません。" + textBox_FilePath.Text);
+                return;
+            }
+
+            // プレビュー
+            {
+                Bitmap bmp = new Bitmap(textBox_FilePath.Text);
+                Bitmap small = new Bitmap(bmp, pictureBox_Image.Width, pictureBox_Image.Height);
+                pictureBox_Image.Image = small;
+            }
+
+            // サムネイル
+            {
+                Bitmap bmp = new Bitmap(textBox_FilePath.Text);
+                Image small = bmp.GetThumbnailImage(pictureBox_Thumnail.Width, pictureBox_Thumnail.Height, null, IntPtr.Zero);
+                pictureBox_Thumnail.Image = small;
+            }
+        }
+    }
+}
