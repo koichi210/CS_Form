e1ed5eb0236987cd1485c2f670ba4d53c61828db
koichi <koichi210@gmail.com>
2020-11-29 09:16:17 +0900

    Add files via upload

---
+++ WebBrowser/WebBrowser/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.button_Go = new System.Windows.Forms.Button();
+            this.textBox_Url = new System.Windows.Forms.TextBox();
+            this.button_Test = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(0, 26);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(476, 409);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // button_Go
+            // 
+            this.button_Go.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_Go.Location = new System.Drawing.Point(399, 1);
+            this.button_Go.Name = "button_Go";
+            this.button_Go.Size = new System.Drawing.Size(29, 19);
+            this.button_Go.TabIndex = 1;
+            this.button_Go.Text = "Go";
+            this.button_Go.UseVisualStyleBackColor = true;
+            this.button_Go.Click += new System.EventHandler(this.button_Go_Click);
+            // 
+            // textBox_Url
+            // 
+            this.textBox_Url.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Url.Location = new System.Drawing.Point(1, 1);
+            this.textBox_Url.Name = "textBox_Url";
+            this.textBox_Url.Size = new System.Drawing.Size(398, 19);
+            this.textBox_Url.TabIndex = 2;
+            // 
+            // button_Test
+            // 
+            this.button_Test.Location = new System.Drawing.Point(434, 1);
+            this.button_Test.Name = "button_Test";
+            this.button_Test.Size = new System.Drawing.Size(42, 19);
+            this.button_Test.TabIndex = 3;
+            this.button_Test.Text = "Test";
+            this.button_Test.UseVisualStyleBackColor = true;
+            this.button_Test.Click += new System.EventHandler(this.button_Test_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 435);
+            this.Controls.Add(this.button_Test);
+            this.Controls.Add(this.textBox_Url);
+            this.Controls.Add(this.button_Go);
+            this.Controls.Add(this.webBrowser);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "WebBrowser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button button_Go;
+        private System.Windows.Forms.TextBox textBox_Url;
+        private System.Windows.Forms.Button button_Test;
+    }
+}
+
---
+++ WebBrowser/WebBrowser/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Icon = Properties.Resources.WebBrowser;
+        }
+
+        private void button_Go_Click(object sender, EventArgs e)
+        {
+            if ( textBox_Url.Text.Length == 0 )
+            {
+                // URLが指定されていない
+                return;
+            }
+
+            //URL読み込み
+            try
+            {
+                webBrowser.Navigate(new Uri(textBox_Url.Text));
+            }
+            catch (System.UriFormatException)
+            {
+                MessageBox.Show("ページが読み込めませんでした" + Environment.NewLine + textBox_Url.Text,
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void button_Test_Click(object sender, EventArgs e)
+        {
+            String str = webBrowser.DocumentText;
+            MessageBox.Show(str, "Source Text");
+        }
+    }
+}
