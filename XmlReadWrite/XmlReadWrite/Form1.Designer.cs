bff1f1130e7be22d196abeed468422de49d460b7
koichi <koichi210@gmail.com>
2020-11-29 09:19:39 +0900

    Add files via upload

---
+++ XmlReadWrite/XmlReadWrite/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace XmlReadWrite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_FileName = new System.Windows.Forms.TextBox();
+            this.button_read1 = new System.Windows.Forms.Button();
+            this.button_write = new System.Windows.Forms.Button();
+            this.textBox_Param1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Param2 = new System.Windows.Forms.TextBox();
+            this.textBox_Param3 = new System.Windows.Forms.TextBox();
+            this.textBox_Param4 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button_read2 = new System.Windows.Forms.Button();
+            this.button_read3 = new System.Windows.Forms.Button();
+            this.button_read4 = new System.Windows.Forms.Button();
+            this.button_write2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_FileName
+            // 
+            this.textBox_FileName.Location = new System.Drawing.Point(13, 24);
+            this.textBox_FileName.Name = "textBox_FileName";
+            this.textBox_FileName.Size = new System.Drawing.Size(195, 19);
+            this.textBox_FileName.TabIndex = 1;
+            // 
+            // button_read1
+            // 
+            this.button_read1.Location = new System.Drawing.Point(120, 203);
+            this.button_read1.Name = "button_read1";
+            this.button_read1.Size = new System.Drawing.Size(88, 23);
+            this.button_read1.TabIndex = 9;
+            this.button_read1.Text = "Read1";
+            this.button_read1.UseVisualStyleBackColor = true;
+            this.button_read1.Click += new System.EventHandler(this.button_read_Click);
+            // 
+            // button_write
+            // 
+            this.button_write.Location = new System.Drawing.Point(26, 203);
+            this.button_write.Name = "button_write";
+            this.button_write.Size = new System.Drawing.Size(88, 23);
+            this.button_write.TabIndex = 8;
+            this.button_write.Text = "Write";
+            this.button_write.UseVisualStyleBackColor = true;
+            this.button_write.Click += new System.EventHandler(this.button_write_Click);
+            // 
+            // textBox_Param1
+            // 
+            this.textBox_Param1.Location = new System.Drawing.Point(14, 73);
+            this.textBox_Param1.Name = "textBox_Param1";
+            this.textBox_Param1.Size = new System.Drawing.Size(194, 19);
+            this.textBox_Param1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Setting";
+            // 
+            // textBox_Param2
+            // 
+            this.textBox_Param2.Location = new System.Drawing.Point(14, 98);
+            this.textBox_Param2.Name = "textBox_Param2";
+            this.textBox_Param2.Size = new System.Drawing.Size(194, 19);
+            this.textBox_Param2.TabIndex = 4;
+            // 
+            // textBox_Param3
+            // 
+            this.textBox_Param3.Location = new System.Drawing.Point(14, 123);
+            this.textBox_Param3.Name = "textBox_Param3";
+            this.textBox_Param3.Size = new System.Drawing.Size(194, 19);
+            this.textBox_Param3.TabIndex = 5;
+            // 
+            // textBox_Param4
+            // 
+            this.textBox_Param4.Location = new System.Drawing.Point(14, 148);
+            this.textBox_Param4.Name = "textBox_Param4";
+            this.textBox_Param4.Size = new System.Drawing.Size(194, 19);
+            this.textBox_Param4.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Xml FilePath";
+            // 
+            // button_read2
+            // 
+            this.button_read2.Location = new System.Drawing.Point(120, 232);
+            this.button_read2.Name = "button_read2";
+            this.button_read2.Size = new System.Drawing.Size(88, 23);
+            this.button_read2.TabIndex = 10;
+            this.button_read2.Text = "Read2";
+            this.button_read2.UseVisualStyleBackColor = true;
+            this.button_read2.Click += new System.EventHandler(this.button_read2_Click);
+            // 
+            // button_read3
+            // 
+            this.button_read3.Location = new System.Drawing.Point(120, 261);
+            this.button_read3.Name = "button_read3";
+            this.button_read3.Size = new System.Drawing.Size(88, 23);
+            this.button_read3.TabIndex = 11;
+            this.button_read3.Text = "Read3";
+            this.button_read3.UseVisualStyleBackColor = true;
+            this.button_read3.Click += new System.EventHandler(this.button_read3_Click);
+            // 
+            // button_read4
+            // 
+            this.button_read4.Location = new System.Drawing.Point(120, 325);
+            this.button_read4.Name = "button_read4";
+            this.button_read4.Size = new System.Drawing.Size(88, 23);
+            this.button_read4.TabIndex = 14;
+            this.button_read4.Text = "Read4";
+            this.button_read4.UseVisualStyleBackColor = true;
+            this.button_read4.Click += new System.EventHandler(this.button_read4_Click);
+            // 
+            // button_write2
+            // 
+            this.button_write2.Location = new System.Drawing.Point(26, 325);
+            this.button_write2.Name = "button_write2";
+            this.button_write2.Size = new System.Drawing.Size(88, 23);
+            this.button_write2.TabIndex = 13;
+            this.button_write2.Text = "Write2";
+            this.button_write2.UseVisualStyleBackColor = true;
+            this.button_write2.Click += new System.EventHandler(this.button_write2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "別のアプローチ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "使えそう";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 360);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button_write2);
+            this.Controls.Add(this.button_read4);
+            this.Controls.Add(this.button_read3);
+            this.Controls.Add(this.button_read2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_Param4);
+            this.Controls.Add(this.textBox_Param3);
+            this.Controls.Add(this.textBox_Param2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Param1);
+            this.Controls.Add(this.button_write);
+            this.Controls.Add(this.button_read1);
+            this.Controls.Add(this.textBox_FileName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_FileName;
+        private System.Windows.Forms.Button button_read1;
+        private System.Windows.Forms.Button button_write;
+        private System.Windows.Forms.TextBox textBox_Param1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_Param2;
+        private System.Windows.Forms.TextBox textBox_Param3;
+        private System.Windows.Forms.TextBox textBox_Param4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button_read2;
+        private System.Windows.Forms.Button button_read3;
+        private System.Windows.Forms.Button button_read4;
+        private System.Windows.Forms.Button button_write2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ XmlReadWrite/XmlReadWrite/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace XmlReadWrite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_FileName.Text = @"Sample.xml";
+            textBox_Param1.Text = @"Hello World";
+            textBox_Param2.Text = "123";
+            textBox_Param3.Text = @"c:\tmp";
+            textBox_Param4.Text = "テスト";
+        }
+
+        private void button_write_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBox_FileName.Text))
+            {
+                MessageBox.Show("ファイルがみつかりません。" + textBox_FileName.Text);
+                return;
+            }
+
+            XmlTextWriter write = null;
+
+            try
+            {
+                write = new XmlTextWriter(textBox_FileName.Text, null);
+                write.WriteStartElement("x", "root", "urn:1");
+                write.WriteStartElement("Setting");
+
+                write.WriteStartElement("set");
+                write.WriteAttributeString("Param", "FileName");
+                write.WriteString(textBox_FileName.Text);
+                write.WriteEndElement();
+
+                write.WriteStartElement("set");
+                write.WriteAttributeString("Param", "Param1");
+                write.WriteString(textBox_Param1.Text);
+                write.WriteEndElement();
+
+                //write.WriteElementString("space", "hoge");
+                //write.WriteElementString("FileName", textBox_FileName.Text);
+                //write.WriteElementString("Param1", textBox_Param1.Text);
+                //write.WriteElementString("Param2", textBox_Param2.Text);
+                //write.WriteElementString("Param3", textBox_Param3.Text);
+                //write.WriteElementString("Param4", textBox_Param4.Text);
+                write.WriteEndElement();
+                write.WriteEndElement();
+                write.Close();
+            }
+
+            finally
+            {
+                if (write != null)
+                {
+                    write.Close();
+                }
+            }
+        }
+
+        private void button_write2_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBox_FileName.Text))
+            {
+                MessageBox.Show("ファイルがみつかりません。" + textBox_FileName.Text);
+                return;
+            }
+
+            XmlDocument document = new XmlDocument();
+            XmlDeclaration declaration = document.CreateXmlDeclaration("1.0", "UTF-8", null);  // XML宣言
+            XmlElement root = document.CreateElement("root");  // ルート要素
+
+            document.AppendChild(declaration);
+            document.AppendChild(root);
+
+            //XmlElement element = document.CreateElement("Setting");
+            //element.InnerText = "text";                     // 要素の内容
+            //element.SetAttribute("attribute", "256");       // 属性
+            //root.AppendChild(element);
+
+            XmlElement element = document.CreateElement("Setting");
+            element.SetAttribute("attribute", "FileName");  // 属性
+            element.InnerText = textBox_FileName.Text;      // 要素の内容
+            root.AppendChild(element);
+
+            element = document.CreateElement("Setting");
+            element.SetAttribute("attribute", "Param1");    // 属性
+            element.InnerText = textBox_Param1.Text;        // 要素の内容
+            root.AppendChild(element);
+
+            element = document.CreateElement("Setting");
+            element.SetAttribute("attribute", "Param2");    // 属性
+            element.InnerText = textBox_Param2.Text;        // 要素の内容
+            root.AppendChild(element);
+
+            element = document.CreateElement("Setting");
+            element.SetAttribute("attribute", "Param3");    // 属性
+            element.InnerText = textBox_Param3.Text;        // 要素の内容
+            root.AppendChild(element);
+
+            element = document.CreateElement("Setting");
+            element.SetAttribute("attribute", "Param4");    // 属性
+            element.InnerText = textBox_Param4.Text;        // 要素の内容
+            root.AppendChild(element);
+
+            // ファイルに保存する
+            document.Save(textBox_FileName.Text);
+        }
+
+        private void button_read_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBox_FileName.Text))
+            {
+                MessageBox.Show("ファイルがみつかりません。" + textBox_FileName.Text);
+                return;
+            }
+
+            XmlTextReader reader = null;
+
+            try
+            {
+                // Load the reader with the data file and ignore all white space nodes.         
+                reader = new XmlTextReader(textBox_FileName.Text);
+                reader.WhitespaceHandling = WhitespaceHandling.None;
+
+                // Parse the file and display each of the nodes.
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            Console.Write("<{0}> ", reader.Name);
+                            break;
+
+                        case XmlNodeType.Text:
+                            Console.Write(reader.Value);
+                            break;
+
+                        case XmlNodeType.CDATA:
+                            Console.Write("<![CDATA[{0}]]>", reader.Value);
+                            break;
+
+                        case XmlNodeType.ProcessingInstruction:
+                            Console.Write("<?{0} {1}?>", reader.Name, reader.Value);
+                            break;
+
+                        case XmlNodeType.Comment:
+                            Console.Write("<!--{0}-->", reader.Value);
+                            break;
+
+                        case XmlNodeType.XmlDeclaration:
+                            Console.Write("<?xml version='1.0'?>");
+                            break;
+
+                        case XmlNodeType.Document:
+                            break;
+
+                        case XmlNodeType.DocumentType:
+                            Console.Write("<!DOCTYPE {0} [{1}]", reader.Name, reader.Value);
+                            break;
+
+                        case XmlNodeType.EntityReference:
+                            Console.Write(reader.Name);
+                            break;
+
+                        case XmlNodeType.EndElement:
+                            Console.Write("</{0}>", reader.Name);
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+        private void button_read2_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBox_FileName.Text))
+            {
+                MessageBox.Show("ファイルがみつかりません。" + textBox_FileName.Text);
+                return;
+            }
+
+            XmlTextReader reader = null;
+
+            try
+            {
+                // Load the reader with the data file and ignore all white space nodes.         
+                reader = new XmlTextReader(textBox_FileName.Text);
+                reader.WhitespaceHandling = WhitespaceHandling.None;
+
+                // Parse the file and display each of the nodes.
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            Console.Write("<{0}> ", reader.Name);
+                            if (reader.Name.Equals("FileName") )
+                            {
+                                Console.Write("<{0}>", reader.ReadElementString(reader.Name));
+                            }
+                            break;
+
+                        default :
+                            break;
+                    }
+                }
+            }
+
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+        private void button_read3_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBox_FileName.Text))
+            {
+                MessageBox.Show("ファイルがみつかりません。" + textBox_FileName.Text);
+                return;
+            }
+
+            XmlTextReader reader = null;
+
+            try
+            {
+                // Load the reader with the data file and ignore all white space nodes.         
+                reader = new XmlTextReader(textBox_FileName.Text);
+                reader.WhitespaceHandling = WhitespaceHandling.None;
+
+                // Parse the file and display each of the nodes.
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        if (reader.HasAttributes)
+                        {
+                            for (int i = 0; i < reader.AttributeCount; i++)
+                            {
+                                // 属性ノードへ移動
+                                reader.MoveToAttribute(i);
+                                // 属性名、及び属性の値を表示
+                                Console.Write("{1} = {2} ", i, reader.Name, reader.Value);
+                            }
+                        }
+                        else
+                        {
+                            Console.Write("{0}",reader.Value);
+                        }
+                    }
+                }
+            }
+
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+        }
+
+        private void button_read4_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(textBox_FileName.Text))
+            {
+                MessageBox.Show("ファイルがみつかりません。" + textBox_FileName.Text);
+                return;
+            }
+
+            XmlDocument document = new XmlDocument();
+
+            // ファイルから読み込む
+            document.Load(textBox_FileName.Text);
+
+            foreach (XmlElement element in document.DocumentElement)
+            {
+                string attribute = element.GetAttribute("attribute");   // 属性
+                string text = element.InnerText;                        // 要素の内容
+
+                if (attribute.Equals("Param1"))
+                {
+                    textBox_Param1.Text = text;
+                }
+                else if (attribute.Equals("Param2"))
+                {
+                    textBox_Param2.Text = text;
+                }
+                else if (attribute.Equals("Param3"))
+                {
+                    textBox_Param3.Text = text;
+                }
+                else if (attribute.Equals("Param4"))
+                {
+                    textBox_Param4.Text = text;
+                }
+            }
+        }
+    }
+}
